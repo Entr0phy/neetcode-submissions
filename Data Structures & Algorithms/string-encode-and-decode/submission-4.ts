452890ffbd7b8cae041462e8edb03217d9d636a6
Lim Pei Ming <77575454+Entr0phy@users.noreply.github.com>
2026-06-26 17:04:18 +0800

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.ts
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        if (strs.length === 0)
+          return 'empty';
+      const res = [];
+      strs.forEach((val)=> {
+        let string = ''
+        for(let i=0; i<val.length;i++) {
+            let value = '0';
+            const ascii = val.charCodeAt(i);
+            if (ascii <10)
+              value = `00${ascii}`
+            else if (ascii <100)
+              value = `0${ascii}`
+            else
+              value = ascii.toString();
+            string+=value
+        }
+        res.push(string);
+      })
+      return res.join();
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        if (str === 'empty')
+          return []
+        const res = [];
+        const array = str.split(',');
+        array.forEach((val)=> {
+          let string = ''
+          for(let i=0;i<val.length;i+=3) {
+            const ascii = `${val[i]}${val[i+1]}${val[i+2]}`
+            string+=String.fromCharCode(+ascii);
+          }
+          res.push(string)
+        })
+        return res;
+    }
+}
