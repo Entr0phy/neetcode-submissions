ce9d1249e3296ed2f312d4141d319cb0b9f53466
Lim Pei Ming <77575454+Entr0phy@users.noreply.github.com>
2026-06-30 15:51:48 +0800

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        const res = [];
+        nums.forEach((val, index) => {
+          let j = index+1;
+          let k = index+2;
+          while(j<nums.length) {
+            if (val + nums[j] + nums[k] ===0) {
+              res.push([val, nums[j], nums[k]]);
+            }
+            if (k===nums.length) {
+                j++;
+                k=j;
+            }
+            k++;
+          }
+        })
+        console.log(res);
+        const set = new Set();
+        const finalRes = [];
+        for (const row of res){
+            const key = row.sort().join();
+            if(!set.has(key)){
+                set.add(key);
+                finalRes.push(row);
+            }
+        }
+        return finalRes;
+    }
+}
